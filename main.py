dde6bcb694d3fe706655f568ad62b44ef7fb7ed4
kanon07 <kanonc515046@gmail.com>
2020-05-18 19:56:35 +0900

    implove

--- main.py
+++ main.py
@@ -4,79 +4,76 @@ import os
 import json
 
 from linebot import (
-        LineBotApi, WebhookHandler
-        )
+    LineBotApi, WebhookHandler
+)
 from linebot.exceptions import (
-        InvalidSignatureError
-        )
+    InvalidSignatureError
+)
 from linebot.models import (
-        MessageEvent, TextMessage, TextSendMessage,
-        )
+    MessageEvent, TextMessage, TextSendMessage,
+)
 
 app = Flask(__name__)
 
-# line api
-    YOUR_CHANNEL_ACCESS_TOKEN = os.environ['YOUR_CHANNEL_ACCESS_TOKEN']
-    YOUR_CHANNEL_SECRET = os.environ['YOUR_CHANNEL_SECRET']
+# LINE API関係の設定値取得
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ['YOUR_CHANNEL_ACCESS_TOKEN']
+YOUR_CHANNEL_SECRET = os.environ['YOUR_CHANNEL_SECRET']
 
-# beebotte
-    YOUR_BEEBOTTE_TOKEN = os.environ['YOUR_BEEBOTTE_TOKEN']
+# Beebotte関係の設定値取得
+YOUR_BEEBOTTE_TOKEN = os.environ['YOUR_BEEBOTTE_TOKEN']
 
-    line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
-#act message
-    on_msg = [s.encode('utf-8') for s in ['on', 'エアコンつけて！']]
-    off_msg = [s.encode('utf-8') for s in ['off', 'エアコン切って！']]
+# 動作を起こすメッセージのリスト
+on_msg = [s.encode('utf-8') for s in ['on', 'エアコンつけて！']]
+off_msg = [s.encode('utf-8') for s in ['off', 'エアコン切って！']]
 
-#line notiv
-    def broadcast_line_msg(msg):
-        line_bot_api.broadcast(TextSendMessage(text=msg))
+# LINEに通知メッセージを送る
+def broadcast_line_msg(msg):
+    line_bot_api.broadcast(TextSendMessage(text=msg))
 
-#mqtt republish
-        def publish_aircon_control_msg(msg):
-            publish.single('JARVIS/sakuravpn', \
+# エアコン制御用のMQTTをパブリッシュする
+def publish_aircon_control_msg(msg):
+    publish.single('my_home/aircon_control', \
                     msg, \
                     hostname='mqtt.beebotte.com', \
                     port=8883, \
                     auth = {'username':'token:{}'.format(YOUR_BEEBOTTE_TOKEN)}, \
                     tls={'ca_certs':'mqtt.beebotte.com.pem'})
 
-            @app.route('/callback', methods=['POST'])
-            def callback():
-# get X-Line-Signature header value
-                signature = request.headers['X-Line-Signature']
+@app.route('/callback', methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
 
-# get request body as text
-body = request.get_data(as_text=True)
+    # get request body as text
+    body = request.get_data(as_text=True)
     app.logger.info('Request body: ' + body)
 
-# handle webhook body
+    # handle webhook body
     try:
-handler.handle(body, signature)
+        handler.handle(body, signature)
     except InvalidSignatureError:
-abort(400)
+        abort(400)
 
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
-    def handle_message(event):
-        msg = event.message.text.encode('utf-8')
+def handle_message(event):
+    msg = event.message.text.encode('utf-8')
 
-        if msg in on_msg:
+    if msg in on_msg:
         publish_aircon_control_msg('on')
-        elif msg in off_msg:
+    elif msg in off_msg:
         publish_aircon_control_msg('off')
-        else:
+    else:
         broadcast_line_msg('\n'.join(['エアコンをつけたい時：', \
-                    *['['+s.decode('utf-8')+']' for s in on_msg], \
-                    '\nエアコンを消したい時：', \
-                    *['['+s.decode('utf-8')+']' for s in off_msg] , \
-                    '\nって話しかけてね！']))
+                                     *['['+s.decode('utf-8')+']' for s in on_msg], \
+                                     '\nエアコンを消したい時：', \
+                                     *['['+s.decode('utf-8')+']' for s in off_msg] , \
+                                     '\nって話しかけてね！']))
 
-        if __name__ == '__main__':
-        port = int(os.getenv('PORT'))
-        app.run(host='0.0.0.0', port=port)
-        )
-        )
-        )
+if __name__ == '__main__':
+    port = int(os.getenv('PORT'))
+    app.run(host='0.0.0.0', port=port)
