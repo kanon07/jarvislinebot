2afb99d1ec5468141cca21c4d213d3408f02c864
kanon07 <kanonc515046@gmail.com>
2020-05-19 02:03:04 +0900

    implove

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@ git commit -m "implove"
 
 expect -c "
 spawn git push
-expect "Username"
-send "kanon07\n"
-expect "password"
-send "wakamekanonn13\n"
+expect \"Username\"
+send \"kanon07\n\"
+expect \"password\"
+send \"wakamekanonn13\n\"
 "
