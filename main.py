da092c00b638506e56c65b204a7bfbbcf238b49f
kanon711 <kanonc515046@gmail.com>
2020-05-18 23:52:52 +0900

    implove

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-# -*- coding: utf-8 -*-
 from flask import Flask, request, abort
+import paho.mqtt.publish as publish
 import os
+import json
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -14,25 +15,41 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-#環境変数取得
-YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
-YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+# LINE API関係の設定値取得
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ['YOUR_CHANNEL_ACCESS_TOKEN']
+YOUR_CHANNEL_SECRET = os.environ['YOUR_CHANNEL_SECRET']
+
+# Beebotte関係の設定値取得
+YOUR_BEEBOTTE_TOKEN = os.environ['YOUR_BEEBOTTE_TOKEN']
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
-@app.route("/")
-def hello_world():
-    return "hello world!"
+# 動作を起こすメッセージのリスト
+on_msg = [s.encode('utf-8') for s in ['on', 'エアコンつけて！']]
+off_msg = [s.encode('utf-8') for s in ['off', 'エアコン切って！']]
 
-@app.route("/callback", methods=['POST'])
+# LINEに通知メッセージを送る
+def broadcast_line_msg(msg):
+    line_bot_api.broadcast(TextSendMessage(text=msg))
+
+# エアコン制御用のMQTTをパブリッシュする
+def publish_aircon_control_msg(msg):
+    publish.single('my_home/aircon_control', \
+                    msg, \
+                    hostname='mqtt.beebotte.com', \
+                    port=8883, \
+                    auth = {'username':'token:{}'.format(YOUR_BEEBOTTE_TOKEN)}, \
+                    tls={'ca_certs':'mqtt.beebotte.com.pem'})
+
+@app.route('/callback', methods=['POST'])
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
     # get request body as text
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    app.logger.info('Request body: ' + body)
 
     # handle webhook body
     try:
@@ -44,11 +61,19 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    msg = event.message.text.encode('utf-8')
 
-if __name__ == "__main__":
-#    app.run()
-    port = int(os.getenv("PORT"))
-    app.run(host="0.0.0.0", port=port)
+    if msg in on_msg:
+        publish_aircon_control_msg('on')
+    elif msg in off_msg:
+        publish_aircon_control_msg('off')
+    else:
+        broadcast_line_msg('\n'.join(['エアコンをつけたい時：', \
+                                     *['['+s.decode('utf-8')+']' for s in on_msg], \
+                                     '\nエアコンを消したい時：', \
+                                     *['['+s.decode('utf-8')+']' for s in off_msg] , \
+                                     '\nって話しかけてね！']))
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT'))
+    app.run(host='0.0.0.0', port=port)
